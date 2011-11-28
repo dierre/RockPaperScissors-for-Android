47300d9bc4f2c3f57f4aba64b6616f4a363853c8
Francesco De Rose <francesco.derose@gmail.com>
2011-11-28 17:47:15 +0100

    First commit

---
+++ src/fdr/rockpaperscissor/RockPaperScissorActivity.java
@@ -0,0 +1,13 @@
+package fdr.rockpaperscissor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RockPaperScissorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
