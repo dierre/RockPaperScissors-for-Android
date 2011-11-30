0a88031bdbd5221dd89f5f0579bb2dcb6dd9dfb5
Francesco De Rose <francesco.derose@gmail.com>
2011-11-30 18:31:57 +0100

    Improved gameplay

--- src/fdr/rockpaperscissor/RockPaperScissorActivity.java
+++ src/fdr/rockpaperscissor/RockPaperScissorActivity.java
@@ -2,11 +2,14 @@ package fdr.rockpaperscissor;
 
 import fdr.rockpaperscissor.model.Game;
 import fdr.rockpaperscissor.model.Game.Move;
+import fdr.rockpaperscissor.model.Game.Result;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RockPaperScissorActivity extends Activity {
 	
@@ -26,6 +29,8 @@ public class RockPaperScissorActivity extends Activity {
 			public void onClick(View v) {
 				Button self = (Button) v;
 				model.setPlayerAMove(Move.valueOf((String) self.getTag()));
+				
+				refreshWidgetContent();
 			}
 		};
         findViewById(R.id.rockButton).setOnClickListener(buttonListener);
@@ -33,9 +38,43 @@ public class RockPaperScissorActivity extends Activity {
         findViewById(R.id.scissorsButton).setOnClickListener(buttonListener);
     }
     
-    public void refreshScores() {
-    	findViewById(R.id.playerScore);
-    	findViewById(R.id.opponentScore);
+    public void refreshWidgetContent() {
+    	((TextView) findViewById(R.id.playerScore)).setText(Integer.toString(model.getPlayerAScore()));
+    	((TextView) findViewById(R.id.opponentScore)).setText(Integer.toString(model.getPlayerBScore()));
+    	
+    	ImageView playerImageView = (ImageView) findViewById(R.id.playerMoveView);
+    	Move playerMove = model.getPlayerAMove();
+    	updateMoveImage(playerImageView, playerMove);
+
+    	ImageView opponentImageView = (ImageView) findViewById(R.id.opponentMoveView);
+    	Move opponentMove = model.getPlayerBMove();
+    	updateMoveImage(opponentImageView, opponentMove);
+    	
+    	Result result = model.getResult();
+    	TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+    	switch (result) {
+		case PLAYER_A_WINS:
+			resultTextView.setText("You win!");
+			break;
+		case PLAYER_B_WINS:
+			resultTextView.setText("Your opponent wins!");
+			break;
+		case TIE:
+			resultTextView.setText("It's a tie!");
+			break;
+		default:
+			break;
+		}
+    	
     }
+
+	private void updateMoveImage(ImageView playerImageView, Move playerMove) {
+		if (playerMove == Move.ROCK)
+			playerImageView.setImageResource(R.drawable.rock);
+		else if (playerMove == Move.PAPER)
+			playerImageView.setImageResource(R.drawable.paper);
+		else if (playerMove == Move.SCISSORS)
+			playerImageView.setImageResource(R.drawable.scissors);
+	}
     
 }
--- src/fdr/rockpaperscissor/model/Game.java
+++ src/fdr/rockpaperscissor/model/Game.java
@@ -1,6 +1,7 @@
 package fdr.rockpaperscissor.model;
 
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 import android.util.Pair;
@@ -11,18 +12,35 @@ public class Game {
 
 	private Move playerAMove;
 	private Move playerBMove;
+	private int  playerAScore;
+	private int  playerBScore;
 	
 	public synchronized Move getPlayerAMove() {
 		return playerAMove;
 	}
 	public synchronized void setPlayerAMove(Move playerAMove) {
 		this.playerAMove = playerAMove;
+		setPlayerBMove(chooseRandomMove());
+		updateScore();
 	}
 	public synchronized Move getPlayerBMove() {
 		return playerBMove;
 	}
 	public synchronized void setPlayerBMove(Move playerBMove) {
 		this.playerBMove = playerBMove;
+	}
+		
+	private Move chooseRandomMove() {
+		Random rnd = new Random();
+		Move[] moves = Move.values();
+		Move randomMove = moves[rnd.nextInt(moves.length)];
+		return randomMove;
+	}
+	
+	private void updateScore() {
+		Result result = getResult();
+		if (result == Result.PLAYER_A_WINS) playerAScore++;
+		else if (result == Result.PLAYER_B_WINS) playerBScore++;
 	}
 	
 	public Result getResult() {
@@ -42,6 +60,19 @@ public class Game {
 			return Result.PLAYER_A_WINS;
 		else
 			return Result.PLAYER_B_WINS;
+	}
+	
+	public int getPlayerAScore() {
+		return playerAScore;
+	}
+	public void setPlayerAScore(int playerAScore) {
+		this.playerAScore = playerAScore;
+	}
+	public int getPlayerBScore() {
+		return playerBScore;
+	}
+	public void setPlayerBScore(int playerBScore) {
+		this.playerBScore = playerBScore;
 	}
 	
 }
